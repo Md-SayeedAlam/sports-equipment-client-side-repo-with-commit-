b59d3badc8b6d51952d3f9ec44695ccd577a440d
Md-SayeedAlam <muhammadsayeedalam9@gmail.com>
2024-12-04 12:39:30 +0600

    added a env file to hide the firebase config

--- src/firebase/firebase.config.js
+++ src/firebase/firebase.config.js
@@ -6,12 +6,12 @@ import { getAuth } from "firebase/auth";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyCY3OxqBlFsOhtxdYYm7DHTG0rmsFYgaO0",
-  authDomain: "sports-equipments-client.firebaseapp.com",
-  projectId: "sports-equipments-client",
-  storageBucket: "sports-equipments-client.firebasestorage.app",
-  messagingSenderId: "626418555261",
-  appId: "1:626418555261:web:37f13061335f766cfc1414"
+  apiKey: import.meta.env.VITE_apiKey,
+  authDomain: import.meta.env.VITE_authDomain,
+  projectId: import.meta.env.VITE_projectId,
+  storageBucket: import.meta.env.VITE_storageBucket,
+  messagingSenderId: import.meta.envVITE_messagingSenderId,
+  appId: import.meta.env.VITE_appId,
 };
 
 // Initialize Firebase
