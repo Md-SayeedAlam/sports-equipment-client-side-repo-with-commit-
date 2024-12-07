29470df3ef991853d8ea6459404dc50a48b20471
Md-SayeedAlam <muhammadsayeedalam9@gmail.com>
2024-12-07 13:16:53 +0600

    added React Awesome Reveal in home page

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -8,6 +8,7 @@ import blog3 from '../../../public/mental.jpg'
 import { useLoaderData } from "react-router-dom";
 import Card from "../Card/Card";
 import Theme from "../Pages/Theme";
+import { Fade } from "react-awesome-reveal";
 
 const Home = () => {
   const items = useLoaderData();
@@ -58,21 +59,24 @@ const Home = () => {
       </div>
 
       {/* Product Section */}
-      <h2 className="font-semibold text-2xl text-center">Sports Equipments</h2>
+      <Fade direction="down"><h2 className="font-semibold text-2xl text-center">Sports Equipments</h2></Fade>
+      <Fade direction="left">
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 border-gray-200 mt-10 mb-10">
         {items.slice(0, 6).map((item) => (
           <Card key={item._id} item={item}></Card>
         ))}
       </div>
+      </Fade>
 
       {/* categories section */}
 
-      <section id="categories" className="py-8 bg-base-300">
+      <section className="py-8 bg-base-300">
         <div className="container mx-auto px-4">
-          <h2 className="text-2xl font-bold text-center mb-6">
+          <Fade direction="down"><h2 className="text-2xl font-bold text-center mb-6">
             Sports Categories
-          </h2>
-          <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
+          </h2></Fade>
+         <Fade direction="right">
+         <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
             <div className="p-4 bg-gray-100 shadow-md rounded-lg text-center">
               <h3 className="text-lg font-semibold mb-2">Football</h3>
               <p>
@@ -102,6 +106,7 @@ const Home = () => {
               <p>Get the best running shoes and apparel for your needs..</p>
             </div>
           </div>
+         </Fade>
         </div>
       </section>
 
@@ -109,16 +114,19 @@ const Home = () => {
 
       <section  className="py-8 ">
         <div className="container mx-auto px-4">
-          <h2 className="text-2xl font-bold text-center mb-6">
+          <Fade direction="down"><h2 className="text-2xl font-bold text-center mb-6">
             What Our Customers Say
-          </h2>
+          </h2></Fade>
+          
           <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
+          
             <div className="p-4 bg-gray-100 shadow-md rounded-lg">
               <p className="italic">
                 "Amazing quality and fantastic service , I appreciate them!"
               </p>
               <p className="mt-2 text-right font-bold">- Sumon Mia</p>
             </div>
+            
             <div className="p-4 bg-gray-100 shadow-md rounded-lg">
               <p className="italic">
                 "I love the variety of products available ,I appreciate them!"
@@ -133,18 +141,22 @@ const Home = () => {
               <p className="mt-2 text-right font-bold">- Bulbul Mia</p>
             </div>
           </div>
+          
         </div>
       </section>
 
       {/* extra 2nd section */}
 
+      
       <section className="py-8  card">
+
         <div className="container mx-auto px-4">
           <h2 className="text-2xl font-bold text-center mb-6">
             Latest Blog Posts
           </h2>
           <div className="grid grid-cols-1 md:grid-cols-2  lg:grid-cols-3 gap-6">
             {/* Blog Post-- 1 */}
+            <Fade direction="down">
             <div className="p-4 bg-white shadow-md rounded-lg">
               <img
                 src={blog1}
@@ -166,7 +178,9 @@ const Home = () => {
                 Read More
               </a>
             </div>
+            </Fade>
             {/* Blog Post-- 2 */}
+            <Fade direction="right">
             <div className="p-4 bg-white shadow-md rounded-lg">
               <img
                 src={blog2}
@@ -188,7 +202,9 @@ const Home = () => {
                 Read More
               </a>
             </div>
+            </Fade>
             {/* Blog Post --3 */}
+            <Fade direction="up">
             <div className="p-4 bg-white shadow-md rounded-lg">
               <img
                 src={blog3}
@@ -210,9 +226,11 @@ const Home = () => {
                 Read More
               </a>
             </div>
+            </Fade>
           </div>
         </div>
       </section>
+     
     </div></>
   );
 };
